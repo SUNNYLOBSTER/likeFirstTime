bae4d8c34b1a0918fd33b1f110140108034ad742
gd67YRN <gdyrn67@gmail.com>
2023-09-07 20:46:50 +0900

    09062046 리나 VO에 toString 추가하였습니당

--- FinalProject_VET/src/main/java/com/min/edu/vo/UsersVO.java
+++ FinalProject_VET/src/main/java/com/min/edu/vo/UsersVO.java
@@ -9,4 +9,10 @@ public class UsersVO {
 	private String delflag;
 	private String head;
 	private String shoulder;
+	@Override
+	public String toString() {
+		return "UsersVO [name=" + name + ", addr=" + addr + ", age=" + age + ", email=" + email + ", delflag=" + delflag
+				+ ", head=" + head + ", shoulder=" + shoulder + "]";
+	}
+	
 }
