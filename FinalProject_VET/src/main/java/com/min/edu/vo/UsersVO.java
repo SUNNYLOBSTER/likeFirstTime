9cfd6c3631ea981404abe7bdc8be9b300c127668
820m15 <820m15@SilverBook>
2023-09-08 11:10:41 +0900

    09081110 KJT commit test

--- FinalProject_VET/src/main/java/com/min/edu/vo/UsersVO.java
+++ FinalProject_VET/src/main/java/com/min/edu/vo/UsersVO.java
@@ -17,5 +17,49 @@ public class UsersVO {
 		return "UsersVO [name=" + name + ", addr=" + addr + ", age=" + age + ", email=" + email + ", delflag=" + delflag
 				+ ", head=" + head + ", shoulder=" + shoulder + "]";
 	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddr() {
+		return addr;
+	}
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getDelflag() {
+		return delflag;
+	}
+	public void setDelflag(String delflag) {
+		this.delflag = delflag;
+	}
+	public String getHead() {
+		return head;
+	}
+	public void setHead(String head) {
+		this.head = head;
+	}
+	public String getShoulder() {
+		return shoulder;
+	}
+	public void setShoulder(String shoulder) {
+		this.shoulder = shoulder;
+	}
+	
+	
 	
 }
