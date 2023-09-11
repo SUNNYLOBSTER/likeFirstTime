ced2198771b0a886fc2e932b4fbbc6f4bcd8c9fd
gd67YRN <gdyrn67@gmail.com>
2023-09-11 15:34:04 +0900

    Feat : Dao/Impl add
    테스트

---
+++ FinalProject_VET/src/main/java/com/min/edu/model/mapper/MediChart_DaoImpl.java
@@ -0,0 +1,16 @@
+package com.min.edu.model.mapper;
+
+import org.springframework.stereotype.Repository;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Repository
+@Slf4j
+public class MediChart_DaoImpl implements IMediChart_Dao {
+	
+	@Override
+	public int countPet(String users_id) {
+		return 0;
+	}
+
+}
