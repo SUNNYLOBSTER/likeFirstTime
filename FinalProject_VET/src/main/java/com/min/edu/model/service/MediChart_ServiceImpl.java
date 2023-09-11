792eb1c093a019e3285defcf5d23530ae9c9de9d
gd67YRN <gdyrn67@gmail.com>
2023-09-11 19:34:26 +0900

    Feat : add mapper to configuration etc..
    컨피그에 medichart 매퍼 추가, medichart dao/impl 수정, junit test_유리나 추가

--- FinalProject_VET/src/main/java/com/min/edu/model/service/MediChart_ServiceImpl.java
+++ FinalProject_VET/src/main/java/com/min/edu/model/service/MediChart_ServiceImpl.java
@@ -1,6 +1,12 @@
 package com.min.edu.model.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.min.edu.model.mapper.IMediChart_Dao;
+import com.min.edu.vo.PetsInfo_VO;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -8,9 +14,25 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class MediChart_ServiceImpl implements IMediChart_Service {
 	
+	@Autowired
+	private IMediChart_Dao dao;
+	
 	@Override
-	public int countPet(String users_id) {
-		return 0;
+	public int countPet(String id) {
+		log.info("&&&&& MediChart_ServiceImpl countPet 전달받은 파라미터값 : {} &&&&&", id);
+		return dao.countPet(id);
+	}
+
+	@Override
+	public List<PetsInfo_VO> searchPet(String id) {
+		log.info("&&&&& MediChart_ServiceImpl searchPet 전달받은 파라미터값 : {} &&&&&", id);
+		return dao.searchPet(id);
+	}
+
+	@Override
+	public int insertNewPet(PetsInfo_VO pvo) {
+		log.info("&&&&& MediChart_ServiceImpl insertNewPet 전달받은 파라미터값 : {} &&&&&", pvo);
+		return dao.insertNewPet(pvo);
 	}
 	
 
