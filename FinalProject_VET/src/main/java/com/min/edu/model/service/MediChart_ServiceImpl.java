df3174f4b849648dfc67d67770030c2424d5965d
YOORINA0819 <134343533+YOORINA0819@users.noreply.github.com>
2023-09-12 09:42:52 +0900

    Ri na (#56)
    * Feat : Add MediChart_Statement
    
    매퍼 추가
    
    * Feat : add mapper to configuration etc..
    
    컨피그에 medichart 매퍼 추가, medichart dao/impl 수정, junit test_유리나 추가
    
    * Feat : modify Configuration.xml etc..
    
    medi_chart 관련 DAO, Impl 수정 및 service로 junit 테스트 진행
    
    ---------
    
    Co-authored-by: SUNNYLOBSTER <134343578+SUNNYLOBSTER@users.noreply.github.com>

--- FinalProject_VET/src/main/java/com/min/edu/model/service/MediChart_ServiceImpl.java
+++ FinalProject_VET/src/main/java/com/min/edu/model/service/MediChart_ServiceImpl.java
@@ -1,11 +1,15 @@
 package com.min.edu.model.service;
 
 import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.min.edu.model.mapper.IMediChart_Dao;
+import com.min.edu.vo.MediChart_VO;
 import com.min.edu.vo.PetsInfo_VO;
 
 import lombok.extern.slf4j.Slf4j;
@@ -33,6 +37,68 @@ public class MediChart_ServiceImpl implements IMediChart_Service {
 	public int insertNewPet(PetsInfo_VO pvo) {
 		log.info("&&&&& MediChart_ServiceImpl insertNewPet 전달받은 파라미터값 : {} &&&&&", pvo);
 		return dao.insertNewPet(pvo);
+	}
+
+	@Override
+	public int deletePet(int pet_seq) {
+		log.info("&&&&& MediChart_ServiceImpl deletePet 전달받은 파라미터값 : {} &&&&&", pet_seq);
+		return dao.deletePet(pet_seq);
+	}
+
+	@Override
+	public int insertNewChart(MediChart_VO mvo) {
+		log.info("&&&&& MediChart_ServiceImpl insertNewChart &&&&&");
+		log.info("&&&&& 전달받은 파라미터값 : {} &&&&&", mvo);
+		return dao.insertNewChart(mvo);
+	}
+
+	@Override
+	public List<MediChart_VO> selectAllChart(String medi_id) {
+		log.info("&&&&& MediChart_ServiceImpl selectAllChart &&&&&");
+		log.info("&&&&& 전달받은 파라미터값 : {} &&&&&", medi_id);
+		return dao.selectAllChart(medi_id);
+	}
+
+	@Override
+	public List<MediChart_VO> selectPetChart(Map<String, Object> map) {
+		log.info("&&&&& MediChart_ServiceImpl selectAllChart &&&&&");
+		log.info("&&&&& 전달받은 파라미터값 : {} &&&&&", map);
+		return dao.selectPetChart(map);
+	}
+
+	@Override
+	public List<MediChart_VO> selectLChart(Map<String, Object> map) {
+		log.info("&&&&& MediChart_ServiceImpl selectLChart &&&&&");
+		log.info("&&&&& 전달받은 파라미터값 : {} &&&&&", map);
+		return dao.selectLChart(map);
+	}
+
+	@Override
+	public List<MediChart_VO> selectSChart(Map<String, Object> map) {
+		log.info("&&&&& MediChart_ServiceImpl selectSChart &&&&&");
+		log.info("&&&&& 전달받은 파라미터값 : {} &&&&&", map);
+		return dao.selectSChart(map);
+	}
+
+	@Override
+	public MediChart_VO selectOneChart(Map<String, Object> map) {
+		log.info("&&&&& MediChart_ServiceImpl selectOneChart &&&&&");
+		log.info("&&&&& 전달받은 파라미터값 : {} &&&&&", map);
+		return dao.selectOneChart(map);
+	}
+
+	@Override
+	public int modifyChart(Map<String, Object> map) {
+		log.info("&&&&& MediChart_ServiceImpl modifyChart &&&&&");
+		log.info("&&&&& 전달받은 파라미터값 : {} &&&&&", map);
+		return dao.modifyChart(map);
+	}
+
+	@Override
+	public int deleteChart(String medi_num) {
+		log.info("&&&&& MediChart_ServiceImpl deleteChart &&&&&");
+		log.info("&&&&& 전달받은 파라미터값 : {} &&&&&", medi_num);
+		return dao.deleteChart(medi_num);
 	}
 	
 
