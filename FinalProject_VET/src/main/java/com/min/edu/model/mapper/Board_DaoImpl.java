e075405ee9bf5b271948d43c725c6e04af2241a7
820m15 <134343563+820m15@users.noreply.github.com>
2023-09-13 13:36:10 +0900

    Jun tae (#67)
    * Fix : 에러 수정
    
    * 1252 commit

--- FinalProject_VET/src/main/java/com/min/edu/model/mapper/Board_DaoImpl.java
+++ FinalProject_VET/src/main/java/com/min/edu/model/mapper/Board_DaoImpl.java
@@ -1,9 +1,26 @@
 package com.min.edu.model.mapper;
 
 
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.min.edu.vo.QuestBoard_VO;
 
 @Repository
 public class Board_DaoImpl implements IBoard_Dao {
+	
+	private static final String NS = "com.min.edu.model.mapper.Board_DaoImpl";
+	
+	@Autowired
+	SqlSessionTemplate session;
+	
+
+	@Override
+	public List<QuestBoard_VO> selectQuest() {
+		return session.selectList(NS+"selectQuest");
+	}
 
 }
