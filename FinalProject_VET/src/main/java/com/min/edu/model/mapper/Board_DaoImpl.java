f9cafd3d22394fb2d54f23930b6cad048a32ee8e
820m15 <134343563+820m15@users.noreply.github.com>
2023-09-13 18:42:04 +0900

    Jun tae (#71)
    * Fix : 에러 수정
    
    * 1252 commit
    
    * 에러수정중
    
    * FIX: 서버 기동을 위한 QuestionBoard_Statement의 alias 작성
    
    * TEST: 진료문의게시판 JUNIT TEST
    BUILD: Board_Controller 및 Board_DaoImpl 내 메소드 생성
    FIX: 일부 쿼리문 수정

--- FinalProject_VET/src/main/java/com/min/edu/model/mapper/Board_DaoImpl.java
+++ FinalProject_VET/src/main/java/com/min/edu/model/mapper/Board_DaoImpl.java
@@ -2,6 +2,7 @@ package com.min.edu.model.mapper;
 
 
 import java.util.List;
+import java.util.Map;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +13,7 @@ import com.min.edu.vo.QuestBoard_VO;
 @Repository
 public class Board_DaoImpl implements IBoard_Dao {
 	
-	private static final String NS = "com.min.edu.model.mapper.Board_DaoImpl";
+	private static final String NS = "com.min.edu.model.mapper.Board_DaoImpl.";
 	
 	@Autowired
 	SqlSessionTemplate session;
@@ -23,4 +24,105 @@ public class Board_DaoImpl implements IBoard_Dao {
 		return session.selectList(NS+"selectQuest");
 	}
 
+	@Override
+	public List<QuestBoard_VO> selectCodeQuest(Map<String, Object> map) {
+		return session.selectList(NS+"selectCodeQuest", map);
+	}
+
+	@Override
+	public List<QuestBoard_VO> selectPartQuest(Map<String, Object> map) {
+		return session.selectList(NS+"selectPartQuest", map);
+	}
+
+	@Override
+	public List<QuestBoard_VO> selectAllBoard() {
+		return session.selectList(NS+"selectAllBoard");
+	}
+
+	@Override
+	public List<QuestBoard_VO> selectOneBoard(String seq) {
+		return session.selectList(NS+"selectOneBoard", seq);
+	}
+	
+	@Override
+	public List<QuestBoard_VO> selectReportBoard() {
+		return session.selectList(NS+"selectReportBoard");
+	}
+	
+	@Override
+	public int insertQuest() {
+		
+		return session.insert(NS+"insertQuest");
+	}
+
+	@Override
+	public int updateFastQuest() {
+		
+		return session.update(NS+"updateFastQuest");
+	}
+
+	@Override
+	public int modifyQuest() {
+		
+		return session.update(NS+"modifyQuest");
+	}
+
+	@Override
+	public int reportQuest() {
+		
+		return session.update(NS+"reportQuest");
+	}
+
+	@Override
+	public int deleteQuest() {
+		
+		return session.update(NS+"deleteQuest");
+	}
+
+	@Override
+	public int insertReply() {
+		return session.insert(NS+"insertReply");
+	}
+
+	@Override
+	public int modifyReply() {
+		return session.update(NS+"modifyReply");
+	}
+
+	@Override
+	public int deleteReply() {
+		
+		return session.update(NS+"deleteReply");
+	}
+
+	@Override
+	public int reportReply() {
+		return session.update(NS+"reportReply");
+	}
+
+	@Override
+	public int chooseReply() {
+		
+		return session.update(NS+"chooseReply");
+	}
+
+	@Override
+	public int countReply(String id) {
+		
+		return session.selectOne(NS+"countReply", id);
+	}
+
+	@Override
+	public int countChosenReply(String id) {
+		return session.selectOne(NS+"countChosenReply", id);
+	}
+
+	@Override
+	public int calChoiceRate(String id) {
+		return session.selectOne(NS+"calChoiceRate", id);
+	}
+
+
+	
+	
 }
