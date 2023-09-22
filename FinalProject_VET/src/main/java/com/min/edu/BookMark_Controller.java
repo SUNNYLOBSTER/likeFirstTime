68a084f93a8760517a07bd684809de4d9314835a
gd67YRN <gdyrn67@gmail.com>
2023-09-22 10:38:42 +0900

    Feat: add funtion on Bookmark
    북마크 다오, 서비스, mapper, junit 테스트 생성

---
+++ FinalProject_VET/src/main/java/com/min/edu/BookMark_Controller.java
@@ -0,0 +1,8 @@
+package com.min.edu;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class BookMark_Controller {
+
+}
---
+++ FinalProject_VET/src/main/java/com/min/edu/model/service/IBookMark_Service.java
@@ -0,0 +1,17 @@
+package com.min.edu.model.service;
+
+import java.util.List;
+
+import com.min.edu.vo.BookMark_VO;
+
+public interface IBookMark_Service {
+	
+	public int countBookmark(String bm_usersid);
+	
+	public List<BookMark_VO> selectAllBookmark(String bm_usersid);
+	
+	public int insertNewBookmark(BookMark_VO bvo);
+	
+	public int deleteBookmark(BookMark_VO bvo);
+
+}
